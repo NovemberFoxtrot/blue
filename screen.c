18bdde4557dcab3aeeaa7af83a2975fc8dadbfe8
November Foxtrot <you@example.com>
2014-06-02 01:11:11 -0700

    r

--- screen.c
+++ screen.c
@@ -12,18 +12,78 @@
 #define GREEN 750
 #define BLUE 750
 
+struct Rock
+{
+	int x;
+	int y;
+
+	int next_x;
+	int next_y;
+
+	int direction_x;
+	int direction_y;
+
+	char *ch;
+};
+
+struct Rock *Rock_create(char *ch)
+{
+	struct Rock *r = malloc(sizeof(struct Rock));
+
+	if (!r) {
+		printf("malloc error");
+		exit(1);
+	}
+
+	r->x = rand() % 10;
+	r->y = rand() % 10;
+
+	r->next_x = 0;
+	r->next_y = 0;
+
+	r->direction_x = 1;
+	r->direction_y = 1;
+
+	r->ch = ch;
+
+	return r;
+}
+
+void Rock_move(struct Rock *r, int max_x, int max_y)
+{
+	r->next_x = r->x + r->direction_x;
+	r->next_y = r->y + r->direction_y;
+
+	if (r->next_x >= max_x || r->next_x < 0) {
+		r->direction_x *= -1;
+	} else {
+		r->x += r->direction_x;
+	}
+
+	if (r->next_y >= max_y || r->next_y < 0) {
+		r->direction_y *= -1;
+	} else {
+		r->y += r->direction_y;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	int x = 0, y = 0;
-	int max_y = 0, max_x = 0;
-	int next_x = 0;
-	int next_y = 0;
-	int direction_x = 1;
-	int direction_y = 1;
+	int max_y = 0;
+	int max_x = 0;
+
+	struct Rock *ship = Rock_create("S");
+	struct Rock *rock = Rock_create("R");
+
+	if (!rock) {
+		printf("malloc error");
+		exit(1);
+	}
 
 	setlocale(LC_ALL, "");
 
 	initscr();
+
 	refresh();
 	cbreak();
 	noecho();
@@ -46,6 +106,7 @@ int main(int argc, char *argv[])
 	init_pair(8, COLOR_CYAN, COLOR_BLACK);
 
 	color_set(0, NULL);
+
 	assume_default_colors(COLOR_WHITE, COLOR_BLACK);
 
 	clear();
@@ -54,17 +115,9 @@ int main(int argc, char *argv[])
 
 	getmaxyx(stdscr, max_y, max_x);
 
-	char *c = " ";
 	int ch = 0;
-	c = " 𐌏 ";
 
 	srand((unsigned)time(NULL));
-
-	for (int i = 0; i < max_x; i++) {
-		for (int j = 0; j < max_y; j++) {
-			mvaddch(j, i, '#');
-		}
-	}
 
 	refresh();
 
@@ -73,59 +126,49 @@ int main(int argc, char *argv[])
 	while (ch != 'q') {
 		ch = getch();
 
-		if (ch == 'c') {
-			int r = rand() % 8;
-			attrset(A_NORMAL | COLOR_PAIR(r));
-			flash();
-		}
+		clear();
 
-		if (ch == KEY_LEFT) {
-			direction_x = -1;
-		}
+		mvprintw(0, 0, "max_y:%d max_x:%d y:%d x:%d", max_y, max_x, ship->y, ship->x);
 
-		if (ch == KEY_RIGHT) {
-			direction_x = 1;
-		}
-
-		if (ch == KEY_UP) {
-			direction_y = -1;
-		}
-
-		if (ch == KEY_DOWN) {
-			direction_y = 1;
-		}
-
-		mvaddstr(y, 0, c);
-		mvaddstr(0, x, c);
-		mvaddstr(y, x, c);
+		mvaddstr(ship->y, ship->x, ship->ch);
+		mvaddstr(rock->y, rock->x, rock->ch);
 
 		refresh();
 
 		napms(100);
 
-		next_x = x + direction_x;
-		next_y = y + direction_y;
-
-		if (ch == 'd' || (next_x >= max_x || next_x < 0)) {
-			direction_x *= -1;
-			if (x > 0) {
-				int r = rand() % 8;
-				attrset(A_NORMAL | COLOR_PAIR(r));
-			}
-		} else {
-			x += direction_x;
+		if (ch == KEY_LEFT) {
+			ship->direction_y = 0;
+			ship->direction_x = -1;
 		}
 
-		if (next_y >= max_y || next_y < 0) {
-			direction_y *= -1;
-			if (y > 0) {
-				int r = rand() % 8;
-				attrset(A_NORMAL | COLOR_PAIR(r));
-			}
-		} else {
-			y += direction_y;
+		if (ch == KEY_RIGHT) {
+			ship->direction_y = 0;
+			ship->direction_x = 1;
+		}
+
+		if (ch == KEY_UP) {
+			ship->direction_y = -1;
+			ship->direction_x = 0;
+		}
+
+		if (ch == KEY_DOWN) {
+			ship->direction_y = 1;
+			ship->direction_x = 0;
+		}
+
+		Rock_move(ship, max_x, max_y);
+		Rock_move(rock, max_x, max_y);
+
+		if (ship->x == rock->x && ship->y == rock->y) {
+			mvaddstr(0,0,"Dude!");
+		napms(1000);
+			break;
 		}
 	}
+
+	if(ship) free(ship);
+	if(rock) free(rock);
 
 	endwin();
 }
