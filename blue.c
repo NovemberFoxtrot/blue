71d89b8497322fc63a5ec536c1e26400d5b71e44
November Foxtrot <november@fox.trot>
2014-06-12 18:51:56 -0700

    s

--- blue.c
+++ blue.c
@@ -439,4 +439,3 @@ int main()
 
 	endwin();
 }
-
--- blue.h
+++ blue.h
@@ -46,26 +46,24 @@ struct blue_object {
 	char *ch;
 };
 
-int setup_keyboard(void);
-void restore_keyboard(void);
-int update_from_input(void);
-
-void blue_game_run(struct blue_game_state *game_state, struct blue_object **objects);
-struct blue_game_state *blue_game_state_create(void);
-void blue_game_init(struct blue_game_state *game_state);
-void blue_object_background_movement_handler(struct blue_object *o, int max_x, int max_y);
-struct blue_object *blue_object_create(char *ch, enum blue_type type);
-struct blue_object **blue_game_create_objects(struct blue_game_state *game_state);
-void blue_object_move(struct blue_object *o, int max_x, int max_y);
 char get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, float p3_y, float *i_x, float *i_y);
 int blue_object_collide(const struct blue_object *a, const struct blue_object *b);
-void blue_object_input(struct blue_object *o, struct blue_object **rockets, int ch);
+int setup_keyboard(void);
+int update_from_input(void);
+struct blue_game_state *blue_game_state_create(void);
 struct blue_object **blue_array_create(uint32_t array_size);
+struct blue_object **blue_game_create_objects(struct blue_game_state *game_state);
+struct blue_object *blue_object_create(char *ch, enum blue_type type);
 void blue_array_clean(struct blue_object **array, uint32_t array_size);
 void blue_array_destroy(struct blue_object **array);
-void blue_render_ship(WINDOW *field, struct blue_object *ship);
+void blue_game_init(struct blue_game_state *game_state);
+void blue_game_run(struct blue_game_state *game_state, struct blue_object **objects);
+void blue_object_background_movement_handler(struct blue_object *o, int max_x, int max_y);
+void blue_object_input(struct blue_object *o, struct blue_object **rockets, int ch);
+void blue_object_move(struct blue_object *o, int max_x, int max_y);
 void blue_render_rock(WINDOW *field, struct blue_object *rock);
-
+void blue_render_ship(WINDOW *field, struct blue_object *ship);
 void on_timer(int signum);
+void restore_keyboard(void);
 
 #endif
